4a2f36b58288e7e8f6d3ba1ac3eecb87e942a9e7
Hiero <kazsik.adam@gmail.com>
2016-01-16 18:11:32 +0100

    Add logger

--- src/main/java/com/hiero/controllers/CarController.java
+++ src/main/java/com/hiero/controllers/CarController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +19,17 @@ import com.hiero.service.CarService;
 @Controller
 public class CarController {
 	
+	static Logger log = Logger.getLogger(CarController.class.getName());
+	
 	@Autowired
 	private CarService carService;
  
 	@RequestMapping("/car/list")
 	public void carList(Model model) {
+		
 		List<Car> carList = carService.findAll();
 		model.addAttribute("carList", carList);
+		log.info("Show actual cars!");
 	}
 	
 	//@RequestMapping("/car/list")
@@ -37,17 +42,19 @@ public class CarController {
 	
 	@RequestMapping("/car/add")
 	public void carAdd() {
-		
+		log.info("Show new car creator form!");
 	}
 	
 	@RequestMapping(value="/car/add", method=RequestMethod.POST)
 	public String carAddSubmit(@ModelAttribute("car") @Valid Car car, BindingResult result) {
 		if(result.hasErrors()) {
 			// show the form again, with the errors
+			log.info("Car creation has failed!");
 			return "car/add";
 		}
 		// validation was successful
 		carService.add(car);
+		log.info("Car creation was succesful!");
 		return "redirect:/car/list";
 	}
 }
