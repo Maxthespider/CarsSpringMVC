7bcf7feb170d01ff78de7f2590ba9b8140941bc4
Hiero <kazsik.adam@gmail.com>
2016-01-16 17:02:21 +0100

    Add form validation

--- src/main/java/com/hiero/controllers/CarController.java
+++ src/main/java/com/hiero/controllers/CarController.java
@@ -2,9 +2,12 @@ package com.hiero.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,19 +21,19 @@ public class CarController {
 	@Autowired
 	private CarService carService;
  
-	//@RequestMapping("/car/list")
-	//public void carList(Model model) {
-	//	List<Car> carList = carService.findAll();
-	//	model.addAttribute("carList", carList);
-	//}
-	
 	@RequestMapping("/car/list")
-	public String carList(Model model){
+	public void carList(Model model) {
 		List<Car> carList = carService.findAll();
 		model.addAttribute("carList", carList);
-		
-		return "carList";
 	}
+	
+	//@RequestMapping("/car/list")
+	//public String carList(Model model){
+	//	List<Car> carList = carService.findAll();
+	//	model.addAttribute("carList", carList);
+	//	
+	//	return "carList";
+	//}
 	
 	@RequestMapping("/car/add")
 	public void carAdd() {
@@ -38,7 +41,12 @@ public class CarController {
 	}
 	
 	@RequestMapping(value="/car/add", method=RequestMethod.POST)
-	public String carAddSubmit(@ModelAttribute Car car) {
+	public String carAddSubmit(@ModelAttribute("car") @Valid Car car, BindingResult result) {
+		if(result.hasErrors()) {
+			// show the form again, with the errors
+			return "car/add";
+		}
+		// validation was successful
 		carService.add(car);
 		return "redirect:/car/list";
 	}
--- src/main/java/com/hiero/model/Car.java
+++ src/main/java/com/hiero/model/Car.java
@@ -1,10 +1,22 @@
 package com.hiero.model;
 
 import java.math.BigDecimal;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
  
 public class Car {
 	
+	@NotEmpty
+	@Size(min=2, max=20)
 	private String name;
+	 
+	@NotNull
+	@Min(1000) @Max(5000000)
 	private BigDecimal price;
 	
 	public String getName() {
@@ -19,6 +31,4 @@ public class Car {
 	public void setPrice(BigDecimal price) {
 		this.price = price;
 	}
- 
-	
 }
